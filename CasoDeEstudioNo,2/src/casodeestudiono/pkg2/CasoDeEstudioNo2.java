60933ec07f6f123f47a19e95f27637d33186d897
sofygomez <144976407+sofygomez@users.noreply.github.com>
2023-12-06 21:02:07 -0600

    CasoEstudioNo.2

---
+++ CasoDeEstudioNo,2/src/casodeestudiono/pkg2/CasoDeEstudioNo2.java
@@ -0,0 +1,27 @@
+
+package casodeestudiono.pkg2;
+
+import javax.swing.JOptionPane;
+
+public class CasoDeEstudioNo2 {
+
+ 
+    public static void main(String[] args) {
+        if (ControlDeGastos())
+            
+            int cuentas[][] = new [6][8];
+        for(int deben=0;deben<arreglo.lenght;deben++){
+            JOptionPane.showMessageDialog(null, "Se solicitan el nombre:"+deben);
+        
+        }
+        
+    
+        
+       
+       }
+    }
+    
+    
+    }//Fun del main
+    
+}//Fin de la clase 
---
+++ CasoDeEstudioNo,2/src/casodeestudiono/pkg2/ControlDeGastos.java
@@ -0,0 +1,56 @@
+
+package casodeestudiono.pkg2;
+
+import javax.swing.JOptionPane;
+
+
+public class ControlDeGastos {
+   
+    private String nombre; 
+    private double loQueDeben; 
+    
+    for(int deben=0;deben<arreglo.lenght;deben++){
+    JOptionPane.showMessageDialog(null, "Se solicita el nombre;"+deben);
+    int cedula = Integer.parseInt(JOptionPane.showInputDialog("Digite el monto de lo que debe:"));
+    arreglo[deben]=new deudor(nombre, montoDeDeuda);
+    
+}
+
+    public ControlDeGastos(String nombre, double loQueDeben, <any> , <any> , deben ) {
+        this.nombre = nombre;
+        this.loQueDeben = loQueDeben;
+        this.<error> = <error>;
+        this.<error> = <error>;
+        this.<error> = <error>;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getLoQueDeben() {
+        return loQueDeben;
+    }
+
+    public void setLoQueDeben(double loQueDeben) {
+        this.loQueDeben = loQueDeben;
+    }
+
+    public int getDeben() {
+        return deben;
+    }
+
+    public void setDeben(int deben) {
+        this.deben = deben;
+    }
+
+
+    
+    
+    
+    
+}//Fin de la clase 
